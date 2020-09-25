a9c8011442a9c56d3e28eb2e8a21eb5f290ebd4a
Manos Serifios <emmanouel.serifios@vodafoneinnovus.com>
2020-09-25 18:19:10 +0300

    Update to @angular/7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
+
+let mainRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent},
+  { path: '404', component: NotFoundComponent},
+  { path: '**', redirectTo: '404'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(mainRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
